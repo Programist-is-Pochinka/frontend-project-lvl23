d0071b85afff1ae38fbfa0462c76a569338525b4
Albert K <rydakopandvoen@gmail.com>
2022-01-24 03:31:43 +0500

    change quotation marks

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -5,22 +5,27 @@ const selectValue = (value) => {
   return newValue;
 };
 
+const createNewValue = (value) => {
+  const newValue = typeof value === 'string' ? `'${value}'` : value;
+  return newValue;
+};
+
 const updateString = (prop, from, to) => {
   if (to === '[complex value]') {
-    return `Property '${prop}' was updated. From '${from}' to [complex value]`;
+    return `Property '${prop}' was updated. From ${createNewValue(from)} to [complex value]`;
   }
   if (from === '[complex value]') {
-    return `Property '${prop}' was updated. From [complex value] to '${to}'`;
+    return `Property '${prop}' was updated. From [complex value] to ${createNewValue(to)}`;
   }
 
-  return `Property '${prop}' was updated. From '${from}' to '${to}'`;
+  return `Property '${prop}' was updated. From ${createNewValue(from)} to ${createNewValue(to)}`;
 };
 
 const addString = (prop, to) => {
   if (to === '[complex value]') {
     return `Property '${prop}' was added with value: [complex value]`;
   }
-  return `Property '${prop}' was added with value: '${to}'`;
+  return `Property '${prop}' was added with value: ${createNewValue(to)}`;
 };
 
 const removeString = (prop) => `Property '${prop}' was removed`;
